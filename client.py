b43f7e01451a8456e75585bc7b634bed08b96a6a
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2015-03-12 15:40:38 +0300

    генерация пачки клиентов

--- client.py
+++ client.py
@@ -13,13 +13,16 @@ logger = logging.getLogger(__name__)
 ch = logging.StreamHandler()
 logger.addHandler(ch)
 logger.setLevel(logging.INFO)
+max_clients = 20
 
 
 class NewClient:
-    def __init__(self, loop):
+    def __init__(self, loop, client_no):
         self.loop = loop
         self.user = 'max'
         self.passwd = 'secret'
+        self.client_no = client_no
+        self.name = self.get_name()
 
     def connection_made(self, transport):
         self.transport = transport
@@ -38,22 +41,26 @@ class NewClient:
 
     def data_received(self, data):
         message = json.loads(data.decode())
-        logger.info('data: %s' % message)
+        logger.info('%s: data: %s' % (self.name, message))
 
-        time.sleep(5)
-        logger.info('sending')
-        self.transport.write('help!'.encode())
-        time.sleep(5)
-        logger.info('sending')
-        self.transport.write('help!'.encode())
+        logger.info('%s: sending name' % self.name)
+        self.transport.write(self.name.encode())
+        logger.info('%s: sending message' % self.name)
+        self.transport.write(str(self.name + ': help!').encode())
 
     def eof_received(self):
         self.transport.close()
 
+    def get_name(self):
+        return 'client%s' % self.client_no
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    coro = loop.create_connection(lambda: NewClient(loop), '127.0.0.1', port=8888)
-    loop.run_until_complete(coro)
+    for i in range(max_clients):
+        coro = loop.create_connection(lambda i=i: NewClient(loop, i),
+                                      '127.0.0.1', port=8888)
+        loop.create_task(coro)
+    time.sleep(3)
     loop.run_forever()
     loop.close()
