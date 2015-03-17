9aac84c7e37dde741bcd96ec64d76777ba05b343
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2015-03-17 12:06:07 +0300

    maxfds - параметр

--- utils.py
+++ utils.py
@@ -12,10 +12,9 @@ def send_msg(sock, msg, *args):
         print(sock)
 
 
-def recv_msg(sock):
+def recv_msg(sock, maxfds=10):
     fds = array.array("i")
     msglen = 4096
-    maxfds = 5
     msg, ancdata, flags, addr = sock.recvmsg(msglen,
                                              socket.CMSG_LEN(maxfds * fds.itemsize))
     for cmsg_level, cmsg_type, cmsg_data in ancdata:
